366d1e5bb998a53b5b2bf6ef25041eeadaed9f89
Yunfeng Wang <wang.14046@buckeyemail.osu.edu>
2025-07-03 22:37:17 -0400

    add screen and memory-history grid

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -9,3 +9,13 @@ eq1.add_op("3");
 eq1.add_op("+");
 eq1.add_op("4");
 document.getElementById("screen").innerText = eq1.to_s();  
+
+function showMemory() {
+  document.getElementById("memory").classList.remove("hidden");
+  document.getElementById("history").classList.add("hidden");
+}
+
+function showHistory() {
+  document.getElementById("history").classList.remove("hidden");
+  document.getElementById("memory").classList.add("hidden");
+}
